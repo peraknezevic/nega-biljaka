0c8654cb24cbeae206052cffa2f80da4afa89c7e
Pera Knezevic <predragknezevic@icloud.com>
2023-03-31 20:06:28 +0200

    Dodate ikone za soc mreze i apdejtovan css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { BsFacebook, BsInstagram } from "react-icons/bs"
 import "./App.css"
 
 function App() {
   return (
     <div className="App">
+      <div className="logo">
+        <a href="https://negabiljaka.com/">Nega Biljaka</a>
+      </div>
       <h1>Vratiće se uskoro</h1>
       <p>U toku su radovi na sajtu.</p>
+      <p>Pratite nas na društvenim mrežama: </p>
+      <p className="socialLinks">
+        <a href="https://www.facebook.com/negabiljaka">
+          <BsFacebook />
+        </a>
+        <a href="https://www.instagram.com/negabiljaka">
+          <BsInstagram />
+        </a>
+      </p>
     </div>
   )
 }
