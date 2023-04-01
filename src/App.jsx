0e600a402221a009025d213c716e0bd5af5ba895
Pera Knezevic <predragknezevic@icloud.com>
2023-04-01 08:20:12 +0200

    Added routing, redirect for Netlify, fixed CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
-import { BsFacebook, BsInstagram } from "react-icons/bs"
-import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./Home"
 
 function App() {
   return (
-    <div className="App">
-      <div className="logo">
-        <a href="https://negabiljaka.com/">Nega Biljaka</a>
-      </div>
-      <h1>Vratiće se uskoro</h1>
-      <p>U toku su radovi na sajtu.</p>
-      <p>Pratite nas na društvenim mrežama: </p>
-      <p className="socialLinks">
-        <a href="https://www.facebook.com/negabiljaka">
-          <BsFacebook />
-        </a>
-        <a href="https://www.instagram.com/negabiljaka">
-          <BsInstagram />
-        </a>
-      </p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/Home.jsx
@@ -0,0 +1,25 @@
+import { BsFacebook, BsInstagram } from "react-icons/bs"
+import "./App.css"
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <div className="logo">
+        <a href="https://negabiljaka.com/">Nega Biljaka</a>
+      </div>
+      <h1>Vratiće se uskoro</h1>
+      <p>U toku su radovi na sajtu.</p>
+      <p>Pratite nas na društvenim mrežama: </p>
+      <p className="socialLinks">
+        <a href="https://www.facebook.com/negabiljaka">
+          <BsFacebook />
+        </a>
+        <a href="https://www.instagram.com/negabiljaka">
+          <BsInstagram />
+        </a>
+      </p>
+    </div>
+  )
+}
+
+export default Home
